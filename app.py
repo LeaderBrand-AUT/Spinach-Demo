77620737f794ccfa75119353202b96e9668ac985
logan-wood <63706140+logan-wood@users.noreply.github.com>
2023-10-11 01:46:37 +1300

    dashboard & view reports
    can now view dashboard and navigate to reports

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, url_for
+import json
 
 app = Flask(__name__)
 
@@ -13,5 +14,15 @@ def login():
 @app.route('/dashboard')
 def dashboard(): 
     return render_template('dashboard.html', title='Dashboard')
+
+@app.route('/reports')
+def reports():
+    # read file
+    with open('./dummydata/reports.json', 'r') as myfile:
+        dummydata = json.load(myfile)
+
+    for report in dummydata:
+        print(report)
+    return render_template('reports.html', reports=dummydata, backButton='dashboard')
 
 app.run(host='0.0.0.0', port=81)
