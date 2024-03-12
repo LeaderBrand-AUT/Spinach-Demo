cec84ee3618ca8796cfcaa6f02a2ac3add3a01c6
logan-wood <logan.loganwood@gmail.com>
2024-03-12 15:19:52 +1300

    Image cropping and resize
    Added a function to crop and resize images in the camera.py file

--- camera.py
+++ camera.py
@@ -3,7 +3,7 @@ import cv2
 ip = '192.168.88.109'
 port = '8080'
 
-rtspLink = 'rtsp://' + ip + ':' + port + '/h264_ulaw.sdp'
+rtspLink = 'http://takemotopiano.aa1.netvolante.jp:8190/nphMotionJpeg?Resolution=640x480&Quality=Standard&Framerate=30'
 
 camera = cv2.VideoCapture(rtspLink)
 print(rtspLink)
@@ -16,4 +16,15 @@ def gen_frames():
         else:
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
+            
             yield (b'---frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def crop_frame(frame, y1, y2, x1, x2):
+    # Constant, size of spinach leaf frames after processing.
+    RESIZE_WIDTH = 1000
+    RESIZE_HEIGHT = 1000
+
+    cropped_frame = frame[y1:y2, x1:x2]
+    resized_frame = cv2.resize(cropped_frame, (RESIZE_WIDTH, RESIZE_HEIGHT), interpolation=cv2.INTER_LINEAR)
+
+    return resized_frame
