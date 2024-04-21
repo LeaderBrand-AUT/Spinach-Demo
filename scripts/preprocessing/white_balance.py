953c5afc6d3d322872fa6e3491a80e8c01b1f483
logan-wood <logan.loganwood@gmail.com>
2024-04-21 16:06:38 +1200

    update mat's work to use constants file

--- scripts/preprocessing/white_balance.py
+++ scripts/preprocessing/white_balance.py
@@ -1,18 +1,11 @@
-# import cv2
-# import numpy as np
-# import sys
-# import skimage
-
-# def white_balancing(frame):
-#     return skimage.img_as_ubyte((frame*1.0 / np.percentile(frame, 99.4, axis=(0, 1))).clip(0, 1))
-
 import cv2
 import numpy as np
 import skimage
 import os
 from skimage import img_as_ubyte
+from ..constants import BRIGHTNESS_FACTOR
 
-def white_balancing(frame, brightness_factor=0.8):
+def white_balancing(frame, brightness_factor=BRIGHTNESS_FACTOR):
     # Normalize the frame based on the 99.4th percentile
     normalized_frame = frame * 1.0 / np.percentile(frame, 99.4, axis=(0, 1))
     
@@ -26,27 +19,3 @@ def white_balancing(frame, brightness_factor=0.8):
     final_image = img_as_ubyte(adjusted_frame)
     
     return final_image
-
-# def process_directory(input_dir, output_dir):
-#     # Ensure output directory exists
-#     if not os.path.exists(output_dir):
-#         os.makedirs(output_dir)
-    
-#     # Process each file in the directory
-#     for filename in os.listdir(input_dir):
-#         if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-#             file_path = os.path.join(input_dir, filename)
-#             frame = cv2.imread(file_path)
-#             if frame is not None:
-#                 processed_image = white_balancing(frame)
-#                 output_file_path = os.path.join(output_dir, f"post-{filename}")
-#                 cv2.imwrite(output_file_path, processed_image)
-#                 print(f"Processed and saved: {output_file_path}")
-#             else:
-#                 print(f"Error: Image not found or path is incorrect for {filename}")
-
-# # Example usage:
-# input_directory = 'input_data'  # Update this path
-# output_directory = 'test'  # Update this path
-# process_directory(input_directory, output_directory)
-
